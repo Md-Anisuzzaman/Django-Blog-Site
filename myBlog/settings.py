4aa4345b4e0bb69d16596b106b1b978647508553
Anisuzzaman <anisuzzaman1199@gmail.com>
2023-09-11 23:14:14 +0600

    remaining changes

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iu$$hl^431r=peyr+k3kygvbh&(stwh(t%z_*kr4*ckb!!a@lz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
